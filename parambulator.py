89ad1c9df8fb58fbf2a29a59ca77887ca15765f7
gsiedschlag <66683975+gsiedschlag@users.noreply.github.com>
2020-06-26 07:00:48 -0700

    Add orbit phasing to calculation
    Defined phasing function to be called after entering second orbit. Phasing allows for rendezvous with other satellites

--- parambulator.py
+++ parambulator.py
@@ -96,7 +96,7 @@ def variable_deltas(r0x,r0y,r0z,v0x,v0y,v0z,Mp,Mo,Isp,variable):
                 inc = math.acos(h0_unit[-1]) #orbit inclination
                 if inc > math.pi/2:
                     inc = inc - math.pi # if inclincation is over 90 degrees use negative equivalent
-                add = [rp,ra,inc,e]
+                add = [rp,ra,inc,e,beta0]
                 orbit_params.append(add)
             
             #Select starting orbit
@@ -113,6 +113,7 @@ def variable_deltas(r0x,r0y,r0z,v0x,v0y,v0z,Mp,Mo,Isp,variable):
                 h = np.linalg.norm(h0[0])
                 e = orbit_params[0][3]
                 r = r0_mag[0]
+                beta = orbit_params[0][4]
             else:
                 rp = orbit_params[0][0]
                 ra = orbit_params[1][1]
@@ -123,6 +124,7 @@ def variable_deltas(r0x,r0y,r0z,v0x,v0y,v0z,Mp,Mo,Isp,variable):
                 h = np.linalg.norm(h0[1])
                 e = orbit_params[1][3]
                 r = r0_mag[1]
+                beta = orbit_params[1][4]
             
             #Calculate velocity of transfer orbit starting at apogee of transfer orbit
             a_t = (rp + ra)/2
@@ -155,23 +157,79 @@ def variable_deltas(r0x,r0y,r0z,v0x,v0y,v0z,Mp,Mo,Isp,variable):
             #Propellant needed to match second orbit
             delta_m2 = Mo1*(1-math.exp(-delta_v2*1000/(9.81*Isp)))# same units as Mo
             
+            #new mass after burn 2
+            Mo2 = Mo1 - delta_m2
+            
             #Total time for travel from starting location of orbit 1 to apogee of orbit 1 +
             #transfer orbit time
             tau1 = 2*math.pi*math.sqrt(a1**3/mu) #period of orbit 1
             E1 = math.acos((a1-r)/(a1*e))
+            
+            #if beta is greater than 90 degrees it is equivalent to being negative
+            #if beta is negative then the satellite is past apogee and affects
+            #eccentric anamoly, E
+            if beta > math.pi/2:
+                E1 = 2*math.pi - E1
+                
             tp1 = math.sqrt(a1**3/mu)*(E1 - e*math.sin(E1)) #time since periapse of orbit 1 in seconds
             time_to_a1 = tau1/2 - tp1 #time needed to reach apogee of orbit 1 in seconds
             
             tau_t = math.pi*math.sqrt(a_t**3/mu) #half period of transfer orbit
+            delta_t = time_to_a1 + tau_t
+            
+            #assuming robot enters new orbit at perigee, do required phasing
+            #to meet up with second satellite
+            delta_v_phasing, phasing_TOF = phasing(a2, delta_t, E1, rp)
+            
+            #Propellant needed for phasing
+            delta_m3 = Mo2*(1-math.exp(-delta_v_phasing*1000/(9.81*Isp)))# same units as Mo
+            
             #populate return matrix based on desired variable choice
-            if variable == 'v':
-                delta_total[point1][point2] = delta_v2 + delta_v_combined
-            elif variable == 'm':
-                delta_total[point1][point2] = delta_m1 + delta_m2
-            else:
-                delta_total[point1][point2] = time_to_a1 + tau_t
+            delta_total[point1][point2] = delta_m1 + delta_m2 + delta_m3
             
             #graph is undirected so populate the columns with the rows
             delta_total[point2][point1] = delta_total[point1][point2]
     
     return delta_total
+
+def phasing(a,delta_t, E, r):
+    '''
+    Parameters
+    ----------
+    a : Positive Float
+        Semimajor axis of orbit to propagate
+    delta_t : Positive Float
+        Change of time in seconds to propogate orbit
+    E : Float
+        Eccentric anamoly of initial position to be moved forward in time
+    r : Float
+        Magnitude of position vector used to calculate the delta V needed to 
+        enter phasing orbit
+
+    Returns
+    -------
+    delta_v_phasing : Positive Float
+        Necessary delta v in km/s for robot to phase within the orbit it is 
+        currently in while taking the least amount of time
+        
+    TOF : Positive Float
+        Time in seconds needed for phasing
+
+    '''
+    
+    import math
+
+    mu = 3.986e5 # Gm of earth in km^3/s^2
+    
+    omega = math.sqrt(mu/a**3)
+    E2 = (E + omega*delta_t)%(2*math.pi)
+    
+    if E2 > math.pi:
+        E2 = - E2
+    a_c = (mu*((2*math.pi-E2)/(2*math.pi*omega))**2)**(1/3) #semimajor axis of phasing orbit
+    t_phasing = (2*math.pi-E2)/omega #time of flight to achieve phasing in seconds
+    
+    #delta v needed to enter phasing orbit and return to original orbit
+    delta_v_phasing = 2*abs(math.sqrt(mu*(2/r-1/a)) - math.sqrt(mu*(2/r-1/a_c))) #km/s
+    
+    return delta_v_phasing, t_phasing
